ed26b5a7ae1a176d9735541819c4876eceb66695
Alex Alex <askon040@gmail.com>
2024-03-01 16:40:48 +0600

    fix 13

---
+++ public/build/assets/home.bed1d6ed.js
@@ -0,0 +1,1 @@
+import{_ as t,A as n}from"./AdminLayout.7c96938c.js";import{r as e,o as s,c}from"./app.983aea31.js";const r={components:{AdminLayout:n}};function a(m,p,_,i,d,f){const o=e("AdminLayout");return s(),c(o)}const A=t(r,[["render",a]]);export{A as default};
