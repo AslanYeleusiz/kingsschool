800b37ed8ebd735090f87d7ae39728a29d5fece6
Alex Alex <askon040@gmail.com>
2024-03-07 20:25:38 +0500

    fix 15

---
+++ database/migrations/2024_03_07_212230_create_card_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('card_id')->on('user.card_id')->cascadeOnDelete();
+            $table->datetime('datetime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card_orders');
+    }
+};
